ae7af0c6bee1f4bb86d876a000ca9ea3602f5c7c
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-03-02 20:45:27 +0700

    clean code

--- ncpp/samples/first_sample/source/first_sample/bootstrap.cpp
+++ ncpp/samples/first_sample/source/first_sample/bootstrap.cpp
@@ -6,20 +6,6 @@ using namespace ncpp;
 
 int main() {
 
-	dop::cbjs({
-
-		dop::job {
-
-			[](dop::job_coroutine& coroutine) {
-
-				
-
-			}
-
-		}
-
-	});
-
 	std::cout << "memory usage: " << memory_usage() << " (bytes)" << std::endl;
 
 	return 0;
