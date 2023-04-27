e1e8c7090e26112efcd388526fe2d3e6f06bdb1d
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-04-27 11:04:15 +0700

    update CMakeLists.txt

--- ncpp/samples/first_sample/source/first_sample/bootstrap.cpp
+++ ncpp/samples/first_sample/source/first_sample/bootstrap.cpp
@@ -6,21 +6,9 @@ using namespace ncpp;
 
 int main() {
 
-	{
 
-#ifdef NCPP_ENABLE_NATIVE_MEMORY_COUNTING
-		std::cout << "native allocated memory: " << native_allocated_memory() << "(bytes)" << std::endl;
-		std::cout << "native used heap memory: " << native_used_heap_memory() << "(bytes)" << std::endl;
-#endif
-		std::cout << std::endl;
 
-	}
 
-#ifdef NCPP_ENABLE_NATIVE_MEMORY_COUNTING
-	std::cout << "native allocated memory: " << native_allocated_memory() << "(bytes)" << std::endl;
-	std::cout << "native used heap memory: " << native_used_heap_memory() << "(bytes)" << std::endl;
-#endif
-	std::cout << std::endl;
 
 	system("pause");
 
