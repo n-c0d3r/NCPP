5a329d3509cdc5db726822a96a48fb9c935aad82
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-05-20 11:23:13 +0700

    remove rtti

--- ncpp/samples/first_sample/source/first_sample/bootstrap.cpp
+++ ncpp/samples/first_sample/source/first_sample/bootstrap.cpp
@@ -4,49 +4,11 @@ using namespace ncpp;
 
 
 
-class A :
-	public rtti::robject_i
-{
-
-	NCPP_RCLASS(A);
-
-
-
-public:
-	A()
-	{
-
-		NCPP_RCSCOPE(A);
-
-	}
-	~A() {
-
-
-
-	}
-
-	NCPP_RCFUNCTION(
-		i32(f32 f), foo
-	);
-
-};
-
-i32 A::foo(f32 f) {
-
-	std::cout << "A " << f << std::endl;
-
-	return 3;
-}
-
-
-
 int main() {
 
 	{
 
-		A a;    
 
-		std::cout << sizeof(a) << std::endl;
 
 		log_memory_stats();
 
