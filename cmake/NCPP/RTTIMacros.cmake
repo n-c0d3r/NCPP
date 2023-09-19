f9cb414947cea2b80bf88f894b1c50f09f70efb7
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-09-19 19:53:35 +0700

    some rtti macros worked

---
+++ cmake/NCPP/RTTIMacros.cmake
@@ -0,0 +1,25 @@
+
+message(STATUS "<NCPP::RTTIMacros> Start building rtti macros")
+
+
+
+include(NCPP/MacroHelper)
+
+
+
+# NCPP_MacroHelper_CreateMacro(
+#     NAME NCPP_ROBJECT
+#     OUTPUT_FILE "${NCPP_GENERATED_FILES_DIR}/rtti_macros.h"
+#     MAX_ADDITIONAL_PARAM_COUNT 256
+#     PARAM_NAMES 
+#         "Options"
+#         "Name"
+#     BODY
+# "\
+
+# "        
+# )
+
+
+
+message(STATUS "<NCPP::RTTIMacros> Build rtti macros done")
--- ncpp/tests/rtti/robject/source/bootstrap.cpp
+++ ncpp/tests/rtti/robject/source/bootstrap.cpp
@@ -6,79 +6,18 @@ using namespace ncpp;
 
 
 
-class B {
+class A {
 
 	NCPP_RCLASS(
-		rtti::default_traits,
-		B,
-
-		NCPP_PUBLIC(i32, i2);
-
-	);
-
-
-
-public:
-	B() {
-
-		i2 = 9;
-
-	}
-	~B() {
-
-
-
-	}
-
-};
-
-
-
-class A : public B {
-
-	NCPP_RCLASS(
-		rtti::default_traits,
+		rtti::default_options,
 		A,
-
-		NCPP_BASE(B);
-
-		NCPP_PUBLIC(i32, i);
-		NCPP_PUBLIC(eastl::string, str);
-
-		NCPP_PUBLIC_VIRTUAL(i32(b8), foo);
-		NCPP_PUBLIC_CONST(i32(b8), foo2);
-
+		PUBLIC(
+			i32,
+			i
+		)
 	);
 
-
-
-public:
-	A() {
-
-		i = 3;
-		str = "Hello World";
-
-	}
-	~A() {
-
-
-
-	}
-
 };
-
-NCPP_DEFINE_VIRTUAL(A, foo, i32, b8) { 
-
-	std::cout << "Hello A" << std::endl;
-	
-	return 0; 
-}
-i32 A::foo2(b8) {
-
-	std::cout << "Hello A 2" << std::endl;
-
-	return 0;
-}
 
 
 
@@ -86,18 +25,11 @@ int main() {
 
 	A a;
 
-	std::cout << a << std::endl;
+	A::rcontainer_type rcontainer;
 
-	const A& ar = a;
-	ar.foo2(true);
+	A::static_reflect(rcontainer);
 
-#ifdef NCPP_ENABLE_RTTI
-	A::rcontext_type ctx;
-	ctx.reflect_t(a);
-	ctx.member("foo").invoke_t<i32(b8)>(&a, true);
-	std::cout << ctx.member("i").cast_to_t<i32>(&a) << std::endl;
-	std::cout << ctx.member("i").is_type_t<i32>() << std::endl;
-#endif
+
 
 	system("pause");
 
