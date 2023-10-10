482e92bd6f3aefbe013aee64387a3cde228608df
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-10-10 23:00:20 +0700

    but F_smart_chunk_storage, F_smart_chunk_adaptor, and F_smart_chunk_allocator (non template version)

--- ncpp/samples/memory_management/chunk_based_allocators/source/bootstrap.cpp
+++ ncpp/samples/memory_management/chunk_based_allocators/source/bootstrap.cpp
@@ -11,9 +11,9 @@ int main() {
 		
 		mem::F_incremental_chunk_allocator incremental_chunk_allocator;
 
-		mem::TF_smart_chunk_storage<> smart_chunk_storage;
-		mem::TF_smart_chunk_adaptor<> smart_chunk_adaptor(&smart_chunk_storage);
-		mem::TF_smart_chunk_allocator<> smart_chunk_allocator(&smart_chunk_adaptor);
+		mem::F_smart_chunk_storage smart_chunk_storage;
+		mem::F_smart_chunk_adaptor smart_chunk_adaptor(&smart_chunk_storage);
+		mem::F_smart_chunk_allocator smart_chunk_allocator(&smart_chunk_adaptor);
 
 		mem::F_default_allocator default_allocator;
 
