1b374e23c3276649a6a3200aa07ab4d4edaf1098
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-10-16 12:18:47 +0700

    update docs and add spinlock, concurrent ring buffer

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # NCPP
 NCoder's Game Engine Learning Aimed C++ Library
 
+---
+
 ## Features
 + Memory management:
   + Incremental chunk allocator
@@ -23,39 +25,49 @@ NCoder's Game Engine Learning Aimed C++ Library
   + Easy to use
   + No memory cost on reflected object
   + Metadata supports 
-  + Flexible
+  + Fast compilation
   + Highly customizable
++ Containers:
+  + Ring buffer
+
+---
 
 ## Core Dependencies
 + EASTL
 + Doxygen (Optional)
+
+---
 
 ## Supported Platforms
   + Windows
   + MacOS
   + Unix Platforms
 
+---
+
 ## Building
   + Windows + Visual Studio 2022:
-    + Step 1: Clone [NCPP git repository](https://github.com/n-c0d3r/ncpp) from GitHub
+    + Step 1: Clone [NCPP git repository](https://github.com/Abytek/ncpp) from GitHub
     + Step 2: Generate Visual Studio 2022 solution by running **scripts/generate_vs2022.bat**
     + Step 3: Navigate to **build/vs2022**
     + Step 4: Open **npp.sln**
     + Step 5: Set **ncpp.samples.hello_ncpp** as startup project
     + Step 6: Run and enjoy!
   + MacOS + Xcode:
-    + Step 1: Clone [NCPP git repository](https://github.com/n-c0d3r/ncpp) from GitHub
+    + Step 1: Clone [NCPP git repository](https://github.com/Abytek/ncpp) from GitHub
     + Step 2: Generate Xcode solution by running **scripts/generate_xcode.sh**
     + Step 3: Navigate to **build/xcode**
     + Step 4: Open **ncpp.xcodeproj**
     + Step 5: Choose **ncpp.samples.hello_ncpp** for current scheme
     + Step 6: Run and enjoy!
   + Unix makefiles:
-    + Step 1: Clone [NCPP git repository](https://github.com/n-c0d3r/ncpp) from GitHub
+    + Step 1: Clone [NCPP git repository](https://github.com/Abytek/ncpp) from GitHub
     + Step 2: Generate makefiles by running **scripts/generate_unix_makefiles.sh**
     + Step 3: execute **scripts/build_unix_makefiles.sh** to build project
     + Step 4: Navigate to **build/unix_makefiles/ncpp/samples/hello_ncpp**
     + Step 5: Run "ncpp.samples.hello_ncpp" and enjoy!
 
+---
+
 ## License
-+ NCPP is licensed under the [MIT license](https://github.com/n-c0d3r/NCPP/blob/main/LICENSE)
++ NCPP is licensed under the [Abytek Software Private License](https://github.com/Abytek/NCPP/blob/main/LICENSE)
--- ncpp/source/ncpp/pac/spinlock.hpp
+++ ncpp/source/ncpp/pac/spinlock.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-/** @file ncpp/containers/concurrent_ring_buffer.hpp
-*	@brief Implements concurrent ring buffer.
+/** @file ncpp/pac/spinlock.hpp
+*	@brief Implement spinlock
 */
 
 
@@ -33,13 +33,6 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <ncpp/utilities/.hpp>
-#include <ncpp/mem/default_allocator.hpp>
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-#include <ncpp/pac/spinlock.hpp>
 
 #pragma endregion
 
@@ -61,168 +54,50 @@
 
 namespace ncpp {
 
-	namespace containers {
+    namespace pac
+    {
 
-		template<typename F_item__, class F_allocator__ = mem::F_default_allocator, class F_lock__ = pac::F_spinlock>
-		class TF_concurrent_ring_buffer {
+        class F_spinlock {
+            
+        private:
+            aflag is_locked_;
+            
+            
+            
+        public:
+            F_spinlock() :
+                is_locked_(ATOMIC_FLAG_INIT)
+            {
+                
+                
+                
+            }
+            ~F_spinlock(){
+                
+                
+                
+            }
+            
+            
+            
+        public:
+            void lock(){
+                
+                while(is_locked_.test_and_set(eastl::memory_order_acquire));
+                
+            }
+            inline b8 try_lock(){
+                
+                return !is_locked_.test_and_set(eastl::memory_order_acquire);
+            }
+            void unlock(){
+                
+                is_locked_.clear(eastl::memory_order_release);
+            }
+            
+        };
 
-		public:
-			using F_item = F_item__;
-			using F_allocator = F_allocator__;
-			using F_lock = F_lock__;
-
-			using F_item_vector = eastl::vector<F_item, F_allocator>;
-
-
-
-		protected:
-			F_item_vector item_vector_;
-			aptrd begin_ = 0;
-			aptrd end_ = 0;
-			
-		private:
-			sz capacity_ = 0;
-			F_lock reader_lock_;
-			F_lock writer_lock_;
-
-
-		public:
-			inline sz size() const { return end_ - begin_; }
-			inline sz capacity() const { return capacity_; }
-			inline bool is_empty() const { return !size(); }
-			inline bool is_null() const { return !capacity_; }
-
-
-
-		public:
-			inline TF_concurrent_ring_buffer() {
-
-
-
-			}
-			inline TF_concurrent_ring_buffer(sz capacity) :
-				capacity_(capacity),
-				item_vector_(capacity)
-			{
-
-				
-
-			}
-
-			inline TF_concurrent_ring_buffer(const TF_concurrent_ring_buffer& x) :
-				item_vector_(x.item_vector_),
-				capacity_(x.capacity_),
-				begin_(x.begin_),
-				end_(x.end_)
-			{
-
-
-
-			}
-			inline TF_concurrent_ring_buffer& operator = (const TF_concurrent_ring_buffer& x) {
-
-				item_vector_ = x.item_vector_;
-				capacity_ = x.capacity_;
-				begin_ = x.begin_;
-				end_ = x.end_;
-
-			}
-
-			inline TF_concurrent_ring_buffer(TF_concurrent_ring_buffer&& x) :
-				item_vector_(std::move(x.item_vector_)),
-				capacity_(x.capacity_),
-				begin_(x.begin_),
-				end_(x.end_)
-			{
-
-
-
-			}
-			inline TF_concurrent_ring_buffer& operator = (TF_concurrent_ring_buffer&& x) {
-
-				item_vector_ = std::move(x.item_vector_);
-				capacity_ = x.capacity_;
-				begin_ = x.begin_;
-				end_ = x.end_;
-
-			}
-
-
-
-		private:
-			template<typename F_passed_item__>
-			void T_push(F_passed_item__&& item) {
-
-				writer_lock_.lock();
-
-				assert(size() < capacity() && "out of capacity");
-
-				item_vector_[(end_.fetch_add(1, eastl::memory_order_release)) % capacity_] = std::forward<F_passed_item__>(item);
-
-				writer_lock_.unlock();
-
-			}
-
-
-
-		public:
-			inline void push(F_item const& item) {
-
-				T_push(std::forward<F_item const&>(item));
-			}
-			inline void push(F_item&& item) {
-
-				T_push(std::forward<F_item>(item));
-			}
-
-			inline F_item pop() {
-
-				reader_lock_.lock();
-
-				ptrd end = end_.load(eastl::memory_order_acquire);
-				ptrd begin = begin_.load(eastl::memory_order_acquire);
-
-				assert((end - begin) > 0 && "ring buffer is empty");
-
-				reader_lock_.unlock();
-				
-				begin_.fetch_add(std::memory_order_release);
-
-				return std::move(item_vector_[begin % capacity_]);
-			}
-
-			inline b8 try_pop(F_item& item) {
-
-				reader_lock_.lock();
-
-				ptrd end = end_.load(eastl::memory_order_acquire);
-				ptrd begin = begin_.load(eastl::memory_order_acquire);
-
-				if ((end - begin)> 0) {
-				
-					begin_.fetch_add(std::memory_order_release);
-
-					item = std::move(item_vector_[begin % capacity_]);
-
-					reader_lock_.unlock();
-
-					return true;
-				}
-
-				reader_lock_.unlock();
-				
-				return false;
-			}
-
-			inline void reset() {
-
-				begin_.store(0, eastl::memory_order_release);
-				end_.store(0, eastl::memory_order_release);
-			}
-
-		};
-
-	}
+    }
 
 }
 
