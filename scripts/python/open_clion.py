7fe82fa79a381982660439967da208395c341d1b
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-11-28 15:56:44 +0700

    update scripts

--- scripts/python/open_clion.py
+++ scripts/python/open_clion.py
@@ -16,12 +16,27 @@ def write_file(path, content):
     with open(path, 'w') as file:
         file.write(content)
 
+def read_file(path):
+    # Check if the file exists
+    if os.path.isfile(path):
+        # Open the file
+        with open(path, 'r') as file:
+            # Read the file
+            data = file.read()
+
+            return data
+    else:
+        print(f"File {path} does not exist")
+
 
 def open_clion():
     # Get the path of this script
     script_path = os.path.realpath(__file__)
     # Get the project root directory (assuming the script is in /project_root/scripts/)
     project_root = os.path.dirname(os.path.dirname(os.path.dirname(script_path)))
+
+    # get project name
+    project_name = read_file(f"{project_root}/.project_name")
 
     clion_exe_command_args = None
     if platform.system() == "Darwin":
@@ -33,13 +48,13 @@ def open_clion():
         raise Exception("CLion executable not found")
 
     # setup project dir
-    project_dir = f"{project_root}/build/clion/ncpp"
+    project_dir = f"{project_root}/build/clion/{project_name}"
     root_cmake_lists_path = f"{project_dir}/CMakeLists.txt"
     root_cmake_lists_content = '''
             cmake_minimum_required(VERSION 3.12)
 
             add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/../../.." "${CMAKE_CURRENT_LIST_DIR}")
-        '''
+        ''' 
     write_file(
         root_cmake_lists_path, 
         root_cmake_lists_content
