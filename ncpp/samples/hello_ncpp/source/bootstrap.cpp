f0d787e6e2929efbcea5890722ba716d5c662502
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-12-02 01:13:57 +0700

    fix gcc compile bug

--- ncpp/samples/hello_ncpp/source/bootstrap.cpp
+++ ncpp/samples/hello_ncpp/source/bootstrap.cpp
@@ -9,6 +9,9 @@ int main() {
 	
     cout << F_string("Hello NCPP ") + NCPP_VERSION_STR << std::endl;
 
+    TF_vector<i32> v = {1,2,3};
+    cout << v << std::endl;
+
     ncpp::pause_console();
 
 	return 0;
