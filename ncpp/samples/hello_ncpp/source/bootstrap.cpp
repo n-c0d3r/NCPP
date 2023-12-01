7345fef72425773fb6af038b1b2c5132b636607a
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-12-01 19:38:32 +0700

    Update bootstrap.cpp

--- ncpp/samples/hello_ncpp/source/bootstrap.cpp
+++ ncpp/samples/hello_ncpp/source/bootstrap.cpp
@@ -9,11 +9,6 @@ int main() {
 	
     cout << F_string("Hello NCPP ") + NCPP_VERSION_STR << std::endl;
 
-    auto a = to_string(3);
-
-    TF_vector<i32> v = {1,2,3};
-    cout << v << std::endl;
-
     ncpp::pause_console();
 
 	return 0;
