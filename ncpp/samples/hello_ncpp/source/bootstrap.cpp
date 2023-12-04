620a242d788c4b86c0880e13cd03a1ed6ed4f500
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-12-05 00:00:07 +0700

    Update bootstrap.cpp

--- ncpp/samples/hello_ncpp/source/bootstrap.cpp
+++ ncpp/samples/hello_ncpp/source/bootstrap.cpp
@@ -8,7 +8,7 @@ using namespace ncpp;
 int main() {
 
     cout << G_string("Hello ")
-         << F_log_color::V_FOREGROUND_BRIGHT_BLUE
+        << F_log_color::V_FOREGROUND_BRIGHT_BLUE
         << G_string("NCPP ")
         << F_log_color::V_FOREGROUND_BRIGHT_CYAN
         << NCPP_VERSION_STR
