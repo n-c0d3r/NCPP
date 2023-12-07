309c70b1b27b63c16f8f3c6d7ab20bdfc4a52edd
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-12-07 12:15:12 +0700

    add NCPP_CREATE and NCPP_DESTROY

--- ncpp/source/ncpp/object.hpp
+++ ncpp/source/ncpp/object.hpp
@@ -54,21 +54,53 @@
 
 namespace ncpp {
 
-    template<typename F_object__, typename F_allocator__ = mem::F_object_allocator, typename... F_args__>
-    NCPP_FORCE_INLINE F_object__* T_create(utilities::TF_no_deduct_t<F_allocator__>* allocator_p, F_args__&&... args) {
+    namespace internal {
 
-        F_object__* object_p = reinterpret_cast<F_object__*>(allocator_p->allocate(sizeof(F_object__), NCPP_ALIGNOF(F_object__), 0, 0));
+        template<typename F_object__, typename F_allocator__ = mem::F_object_allocator>
+        NCPP_FORCE_INLINE F_object__* T_allocate_object(utilities::TF_no_deduct_t<F_allocator__>* allocator_p) {
 
-        return new(object_p) F_object__{ std::forward<F_args__>(args)... };
-    }
+            return reinterpret_cast<F_object__*>(allocator_p->allocate(sizeof(F_object__), eastl::max((sz)NCPP_ALIGNOF(F_object__), (sz)EASTL_ALLOCATOR_MIN_ALIGNMENT), 0, 0));
+        }
 
-    template<typename F_object__, typename F_allocator__ = mem::F_object_allocator>
-    NCPP_FORCE_INLINE void T_delete(utilities::TF_no_deduct_t<F_allocator__>* allocator_p, F_object__* object_p) {
+        template<typename F_object__, typename F_allocator__ = mem::F_object_allocator>
+        NCPP_FORCE_INLINE void T_deallocate_object(utilities::TF_no_deduct_t<F_allocator__>* allocator_p, F_object__* object_p) {
 
-        object_p->~F_object__();
+            allocator_p->deallocate(object_p, 1);
+        }
 
-        allocator_p->deallocate(object_p, 1);
+        template<typename F_object__>
+        NCPP_FORCE_INLINE void T_deconstruct_object(F_object__* object_p) {
+
+            object_p->~F_object__();
+        }
+
     }
 
 }
 
+
+
+#define NCPP_CREATE(AllocatorPointer, ObjectType, ...) ( \
+        new(                                                 \
+            ncpp::internal::T_allocate_object<                         \
+                ObjectType,                                  \
+                ncpp::utilities::TF_nth_template_arg_t<      \
+                    !std::is_same_v<decltype(AllocatorPointer), int>, \
+                    ncpp::mem::F_object_allocator,           \
+                    std::remove_pointer_t<decltype(AllocatorPointer)>  \
+                >                                            \
+            >(AllocatorPointer)                               \
+        ) ObjectType { __VA_ARGS__ }      \
+    )
+
+#define NCPP_DESTROY(AllocatorPointer, ObjectPointer) {\
+        ncpp::internal::T_deconstruct_object<std::remove_pointer_t<decltype(ObjectPointer)>>(ObjectPointer);        \
+        ncpp::internal::T_deallocate_object<                         \
+            std::remove_pointer_t<decltype(ObjectPointer)>,                                  \
+            ncpp::utilities::TF_nth_template_arg_t<      \
+                !std::is_same_v<decltype(AllocatorPointer), int>, \
+                ncpp::mem::F_object_allocator,           \
+                std::remove_pointer_t<decltype(AllocatorPointer)>  \
+            >                                            \
+        >(AllocatorPointer, ObjectPointer);\
+    };
