c70b12f7b8f7045cfd08737bcbc8d1e620372087
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-01-01 22:00:38 +0700

    clean code

--- ncpp/source/ncpp/containers/string_utilities.hpp
+++ ncpp/source/ncpp/containers/string_utilities.hpp
@@ -32,6 +32,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#include <ncpp/utilities/no_deduct.hpp>
 #include <ncpp/containers/eastl_bindings.hpp>
 #include <ncpp/containers/eastl_views.hpp>
 
@@ -58,7 +59,12 @@ namespace ncpp {
     namespace containers {
 
         template<typename F_string__>
-        inline std::enable_if_t<T_is_same_container<F_string__, F_string>, F_string__> T_replace(const F_string__& source, V_string from, V_string to) {
+        requires T_is_same_container<F_string__, F_string>
+        inline F_string__ T_replace(
+            const F_string__& source,
+            utilities::TF_no_deduct<TF_view<F_string__>> from,
+            utilities::TF_no_deduct<TF_view<F_string__>> to
+        ) {
 
             F_string__ result = source;
 
@@ -71,9 +77,22 @@ namespace ncpp {
 
             return result;
         }
+        NCPP_FORCE_INLINE F_string replace(const F_string& source, TF_view<F_string> from, TF_view<F_string> to) {
+
+            return T_replace<F_string>(source, from, to);
+        }
+        NCPP_FORCE_INLINE G_string G_replace(const G_string& source, TF_view<G_string> from, TF_view<G_string> to) {
+
+            return T_replace<G_string>(source, from, to);
+        }
 
         template<typename F_string__>
-        inline std::enable_if_t<T_is_same_container<F_string__, F_string>, F_string__> T_replace_all(const F_string__& source, V_string from, V_string to) {
+        requires T_is_same_container<F_string__, F_string>
+        inline F_string__ T_replace_all(
+            const F_string__& source,
+            utilities::TF_no_deduct<TF_view<F_string__>> from,
+            utilities::TF_no_deduct<TF_view<F_string__>> to
+        ) {
 
             if(from->empty())
                 return source;
@@ -92,11 +111,33 @@ namespace ncpp {
 
             return result;
         }
+        NCPP_FORCE_INLINE F_string replace_all(const F_string& source, TF_view<F_string> from, TF_view<F_string> to) {
+
+            return T_replace_all<F_string>(source, from, to);
+        }
+        NCPP_FORCE_INLINE G_string G_replace_all(const G_string& source, TF_view<G_string> from, TF_view<G_string> to) {
+
+            return T_replace_all<G_string>(source, from, to);
+        }
 
         template<typename F_string__ = F_string>
         inline F_string__ T_decimal_to_hex(i64 decimal_in) {
 
             std::basic_stringstream<typename F_string__::value_type> stream;
+            stream << std::hex << decimal_in;
+
+            return stream.str().c_str();
+        }
+        NCPP_FORCE_INLINE F_string decimal_to_hex(i64 decimal_in) {
+
+            std::basic_stringstream<typename F_string::value_type> stream;
+            stream << std::hex << decimal_in;
+
+            return stream.str().c_str();
+        }
+        NCPP_FORCE_INLINE G_string G_decimal_to_hex(i64 decimal_in) {
+
+            std::basic_stringstream<typename G_string::value_type> stream;
             stream << std::hex << decimal_in;
 
             return stream.str().c_str();
