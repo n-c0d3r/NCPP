a6a484a3f801b240510e74b7d6b3b71809eae7ac
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-01-20 12:33:11 +0700

    remove automatic definition of default object storages

--- ncpp/source/ncpp/object.cpp
+++ ncpp/source/ncpp/object.cpp
@@ -4,12 +4,3 @@
 
 NCPP_DEFINE_SINGLETON(ncpp::TF_default_object_storage<true>);
 NCPP_DEFINE_SINGLETON(ncpp::TF_default_object_storage<false>);
-
-
-
-namespace ncpp {
-
-    TF_default_object_storage<true> default_object_storage(std::thread::hardware_concurrency());
-    TF_default_object_storage<false> default_object_storage_no_thread_safe;
-
-}
