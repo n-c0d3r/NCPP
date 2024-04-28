6d9f8df79a50a34e9f34970da524f653b5fe8bab
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-04-28 13:17:22 +0700

    Update object.hpp

--- ncpp/source/ncpp/object.hpp
+++ ncpp/source/ncpp/object.hpp
@@ -531,7 +531,20 @@ namespace ncpp {
                 NCPP_FORCE_INLINE const TF_bind_requirements<F_requirements_in__>&& T_require() const&& noexcept {\
 \
                     return (const TF_bind_requirements<F_requirements_in__>&&)*this;\
-                }
+                }                                                            \
+			public:                                                                   \
+				template<ncpp::T_is_oref F_other_oref__>\
+				requires T_is_object_down_castable<typename F_oref__::F_passed_object, typename F_other_oref__::F_passed_object>\
+				NCPP_FORCE_INLINE operator TF##NamePrefixConnector##Name<F_other_oref__> () const noexcept {\
+			\
+					return { oref };\
+				}\
+				template<ncpp::T_is_oref F_other_oref__>\
+				requires T_is_object_up_castable<typename F_oref__::F_passed_object, typename F_other_oref__::F_passed_object>\
+				explicit NCPP_FORCE_INLINE operator TF##NamePrefixConnector##Name<F_other_oref__> () const noexcept {\
+			\
+					return { oref };\
+				}
 
 #define NCPP_FHANDLE_GENERATED_BODY(Name, NamePrefixConnector, ...) NCPP_FHANDLE_GENERATED_BODY_ADVANCED(Name,_,__VA_ARGS__)
 
