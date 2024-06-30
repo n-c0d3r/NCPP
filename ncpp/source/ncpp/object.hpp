ba9d724a72fed217aa2d2e82a4c04c3121dc181f
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-06-30 22:06:56 +0700

    Update object.hpp

--- ncpp/source/ncpp/object.hpp
+++ ncpp/source/ncpp/object.hpp
@@ -870,7 +870,7 @@ namespace ncpp {
                     0,
                     0
                 );
-                u32* new_next_p = new_generation_p + size;
+                u32* new_next_p = new_generation_p + new_size;
 
                 if(size) {
 
@@ -1168,7 +1168,7 @@ namespace ncpp {
                     0,
                     0
                 );
-                u32* new_next_p = new_generation_p + size;
+                u32* new_next_p = new_generation_p + new_size;
 
                 if(size) {
 
