1e5a9100ed60ffede8a8afb1142141cda6c94ce8
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-09-21 02:11:22 +0700

    Create AddFileAsDependency.cmake

---
+++ cmake/NCPP/Utilities/AddFileAsDependency.cmake
@@ -0,0 +1,15 @@
+
+include(NCPP/Utilities/TempDependenciesDir)
+
+
+
+function(NCPP_AddTargetFileAsDependency TARGET FILE)
+    NCPP_TempDependenciesDir(${TARGET} TargetTempDependenciesDir)
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory
+            "${TargetTempDependenciesDir}"
+        COMMAND ${CMAKE_COMMAND} -E copy
+            "${FILE}"
+            "${TargetTempDependenciesDir}/"
+    )
+endfunction()
