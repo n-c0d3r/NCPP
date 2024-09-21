41af871842bf0602767755da6590c36142b30cbc
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-09-21 18:44:42 +0700

    Update TempDependenciesDir.cmake

--- cmake/NCPP/Utilities/TempDependenciesDir.cmake
+++ cmake/NCPP/Utilities/TempDependenciesDir.cmake
@@ -1,4 +1,5 @@
 
 function(NCPP_TempDependenciesDir TARGET OUT_VAR)
-    set(${OUT_VAR} "$<TARGET_FILE_DIR:${TARGET}>/___temp_dependencies___" PARENT_SCOPE)
+    get_target_property(BinaryDir ${TARGET} BINARY_DIR)
+    set(${OUT_VAR} "${BinaryDir}/___temp_dependencies___/$<CONFIG>" PARENT_SCOPE)
 endfunction()
