b0b3a0bb3215ddd0db17de3ce763585e8eea8993
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-10-25 02:15:54 +0700

    Update AddFileAsDependency.cmake

--- cmake/NCPP/Utilities/AddFileAsDependency.cmake
+++ cmake/NCPP/Utilities/AddFileAsDependency.cmake
@@ -13,3 +13,14 @@ function(NCPP_AddFileAsDependency TARGET FILE)
             "${TargetTempDependenciesDir}/"
     )
 endfunction()
+
+function(NCPP_AddFileAsDependencyWithOffset TARGET FILE OFFSET)
+    NCPP_TempDependenciesDir(${TARGET} TargetTempDependenciesDir)
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory
+            "${TargetTempDependenciesDir}/${OFFSET}"
+        COMMAND ${CMAKE_COMMAND} -E copy
+            "${FILE}"
+            "${TargetTempDependenciesDir}/${OFFSET}/"
+    )
+endfunction()
